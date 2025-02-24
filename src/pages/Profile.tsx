273a5a70bc686ecd0715933fe0a9909c9734e912
Giancyril <mijaresgiancyril@gmail.com>
2025-02-24 14:31:37 +0800

    Added media cards in Profile

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -7,7 +7,9 @@ import {
       IonTitle, 
       IonToolbar 
   } from '@ionic/react';
-  
+  import React from 'react';
+import { IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle } from '@ionic/react';
+
   const Profile: React.FC = () => {
     return (
       <IonPage>
@@ -20,6 +22,15 @@ import {
           </IonToolbar>
         </IonHeader>
         <IonContent fullscreen>
+        <IonCard>
+      <img alt="Silhouette of mountains" src="https://ionicframework.com/docs/img/demos/card-media.png" />
+      <IonCardHeader>
+        <IonCardTitle>Card Title</IonCardTitle>
+        <IonCardSubtitle>Card Subtitle</IonCardSubtitle>
+      </IonCardHeader>
+
+      <IonCardContent>Here's a small text description for the card content. Nothing more, nothing less.</IonCardContent>
+    </IonCard>
         </IonContent>
       </IonPage>
     );
