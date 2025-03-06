19220de866dd72a459fae21222f3625a19a39cce
Giancyril <mijaresgiancyril@gmail.com>
2025-03-06 15:07:40 +0800

    Updates

--- assets/swipe-back-legacy-i8AWfFZI.js
+++ assets/swipe-back-legacy-i8AWfFZI.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-tnXhqLou.js","./index3-legacy-XqIF9Kbs.js"],(function(e,t){"use strict";var n,r,i;return{setters:[e=>{n=e.i,r=e.c},e=>{i=e.createGesture}],execute:function(){
+System.register(["./index-legacy-CTcQffFm.js","./index3-legacy-D5pMRWMh.js"],(function(e,t){"use strict";var n,r,i;return{setters:[e=>{n=e.i,r=e.c},e=>{i=e.createGesture}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
