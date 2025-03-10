6c0b04d0e9ec42e0f0bbdc6ea6977725eff02887
Giancyril <mijaresgiancyril@gmail.com>
2025-03-10 23:04:14 +0800

    Updates

--- assets/ios.transition-legacy-zIctLpZ8.js
+++ assets/ios.transition-legacy-zIctLpZ8.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-Mm6YLu5u.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.m,n=t.n}],execute:function(){
+System.register(["./index-legacy-DlBZN931.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.m,n=t.n}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
--- assets/swipe-back-legacy-DCZXn7eb.js
+++ assets/swipe-back-legacy-DCZXn7eb.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-Mm6YLu5u.js","./index3-legacy-Cs1_uPY-.js"],(function(e,t){"use strict";var n,r,i;return{setters:[e=>{n=e.i,r=e.c},e=>{i=e.createGesture}],execute:function(){
+System.register(["./index-legacy-DlBZN931.js","./index3-legacy-7R_NLFWk.js"],(function(e,t){"use strict";var n,r,i;return{setters:[e=>{n=e.i,r=e.c},e=>{i=e.createGesture}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
