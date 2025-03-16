4738c7e7edf2009a7d1e739836d6d4cd32c20fce
Giancyril <mijaresgiancyril@gmail.com>
2025-03-16 23:19:53 +0800

    Updates

--- assets/md.transition-legacy-B799w2vK.js
+++ assets/md.transition-legacy-B799w2vK.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-DlBZN931.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.n,i=e.m}],execute:function(){
+System.register(["./index-legacy-Bw0thR_R.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.o,i=e.n}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
