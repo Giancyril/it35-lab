025256a38d30bed661fd031d6267c952603b86d5
Giancyril <mijaresgiancyril@gmail.com>
2025-04-30 20:11:16 +0800

    Updates

--- assets/ios.transition-legacy-ZNPIsALf.js
+++ assets/ios.transition-legacy-ZNPIsALf.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-DQDpKHmw.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
+System.register(["./index-legacy-BjLha1_n.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
